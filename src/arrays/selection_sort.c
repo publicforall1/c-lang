274af2ee6d630d4ed0035a9e88c2f5eb1ac135a4
publicforall1 <publicforall1@gmail.com>
2019-08-04 19:58:36 +0700

    refactor(selection sort): make it more complicated

--- src/arrays/selection_sort.c
+++ src/arrays/selection_sort.c
@@ -4,56 +4,59 @@
  */
 #include <stdio.h>
 #include <string.h>
-#define size_of_array 20
+#include <stdbool.h>
+#define size 20
 
-void selection_sort(int intArray[], int number_of_element, char* mode);
-void swap(int* firstNumber, int* secondNumber);
-void getArray(int intArray[], int number_of_element);
-void printArray(int intArray[], int number_of_element);
+void selection_sort(int arr[], int number_of_element, bool is_asc);
+void swap(int* a, int* b);
+void show_array(int arr[], int number_of_element);
 
 int main(void) {
 
-    int intArray[size_of_array] = {17, 7, 6,  4, 18, 10, 13, 19, 9, 11,
+    int arr[size] = {17, 7, 6,  4, 18, 10, 13, 19, 9, 11,
                                    16, 5, 20, 8, 3,  15, 2,  12, 1, 14};
     printf("Origin Int Array: ");
-    printArray(intArray, size_of_array);
-    selection_sort(intArray, size_of_array, "asc");
+    show_array(arr, size);
+    selection_sort(arr, size, 1);
     printf("Ascending Int Array: ");
-    printArray(intArray, size_of_array);
-    selection_sort(intArray, size_of_array, "des");
+    show_array(arr, size);
+    selection_sort(arr, size, 0);
     printf("Descending Int Array: ");
-    printArray(intArray, size_of_array);
+    show_array(arr, size);
 
     return 0;
 }
 
-void selection_sort(int intArray[], int number_of_element, char* mode) {
-    for (int beforeIndex = 0; beforeIndex < number_of_element - 1;
-         beforeIndex++)
-        for (int afterIndex = beforeIndex + 1; afterIndex < number_of_element;
-             afterIndex++)
-            if (strcmp(mode, "asc") == 0) {
-                if (intArray[afterIndex] < intArray[beforeIndex])
-                    swap(&intArray[afterIndex], &intArray[beforeIndex]);
-            } else if (strcmp(mode, "des") == 0) {
-                if (intArray[afterIndex] > intArray[beforeIndex])
-                    swap(&intArray[afterIndex], &intArray[beforeIndex]);
-            }
-};
+void selection_sort(int arr[], int number_of_element, bool is_asc) {
+    /**
+     * @ Warning:
+     * - Keep calm & read the code.
+     * - is_asc: asc stands for ascending
+     * 
+     */
+    int i = 0, j = 0;
+    for (i = 0; i < number_of_element - 1; i++)
+        for (j = i + 1; j < number_of_element; j++)
+                is_asc 
+                ? 
+                (arr[j] < arr[i] ? swap(&arr[j], &arr[i]) : (void)1)
+                :
+                (arr[j] > arr[i] ? swap(&arr[j], &arr[i]) : (void)1);
 
-void swap(int* firstNumber, int* secondNumber) {
-    int temp = *firstNumber;
-    *firstNumber = *secondNumber;
-    *secondNumber = temp;
-};
+}
 
-void getArray(int intArray[], int number_of_element) {
-    for (int i = 0; i < number_of_element; i++)
-        scanf("%d", &intArray[i]);
-};
+void swap(int* a, int* b) {
+    // using XOR operator
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
+}
 
-void printArray(int intArray[], int number_of_element) {
-    for (int i = 0; i < number_of_element; i++)
-        printf("%d ", intArray[i]);
+void show_array(int arr[], int number_of_element) {
+    int i = 0;
+    while(i < number_of_element){
+        printf("%d ", arr[i]);
+        ++i;
+    }
     printf("\n");
-};
+}
