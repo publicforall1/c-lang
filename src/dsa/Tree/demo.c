699b3257e1dc373d9065eeb65eea475b2d126cc0
taiprogramer <atgtai100@gmail.com>
2019-11-12 09:00:22 +0700

    @add: Tree - Demo

--- src/dsa/Tree/demo.c
+++ src/dsa/Tree/demo.c
@@ -3,43 +3,53 @@
 
 #define PRINT_NEW_LINE printf("\n");
 
-int main(void){
-    
+int main(void) {
+
     Tree tree;
     makenull_tree(&tree);
 
     // Tree in diagram
-    add_node('A', -1, &tree);
-    add_node('B', 0, &tree);
-    add_node('C', 0, &tree);
-    add_node('D', 0, &tree);
-    add_node('E', 1, &tree);
-    add_node('F', 2, &tree);
-    add_node('G', 2, &tree);
-    add_node('H', 3, &tree);
+    add_node_to_tree('A', -1, &tree);
+    add_node_to_tree('B', 0, &tree);
+    add_node_to_tree('C', 0, &tree);
+    add_node_to_tree('D', 0, &tree);
+    add_node_to_tree('E', 1, &tree);
+    add_node_to_tree('F', 2, &tree);
+    add_node_to_tree('G', 2, &tree);
+    add_node_to_tree('H', 3, &tree);
 
-    printf("Left-child of A: %c\n", get_value(left_child(0, tree), tree));
-    printf("Left-child of B: %c\n", get_value(left_child(1, tree), tree));
-    printf("Left-child of C: %c\n", get_value(left_child(2, tree), tree));
-    printf("Left-child of D: %c\n", get_value(left_child(3, tree), tree));
-    printf("Left-child of E: %c\n", get_value(left_child(4, tree), tree));
-    printf("Left-child of F: %c\n", get_value(left_child(5, tree), tree));
-    printf("Left-child of G: %c\n", get_value(left_child(6, tree), tree));
-    printf("Left-child of H: %c\n", get_value(left_child(7, tree), tree));
+    printf("Left-child of A: %c\n",
+           get_value_from_tree(tree_left_child_of(0, tree), tree));
+    printf("Left-child of B: %c\n",
+           get_value_from_tree(tree_left_child_of(1, tree), tree));
+    printf("Left-child of C: %c\n",
+           get_value_from_tree(tree_left_child_of(2, tree), tree));
+    printf("Left-child of D: %c\n",
+           get_value_from_tree(tree_left_child_of(3, tree), tree));
+    printf("Left-child of E: %c\n",
+           get_value_from_tree(tree_left_child_of(4, tree), tree));
+    printf("Left-child of F: %c\n",
+           get_value_from_tree(tree_left_child_of(5, tree), tree));
+    printf("Left-child of G: %c\n",
+           get_value_from_tree(tree_left_child_of(6, tree), tree));
+    printf("Left-child of H: %c\n",
+           get_value_from_tree(tree_left_child_of(7, tree), tree));
 
-    printf("Right-sibling of A: %d\n", right_sibling(0, tree));
-    printf("Right-sibling of B: %d\n", right_sibling(1, tree));
-    printf("Right-sibling of C: %d\n", right_sibling(2, tree));
-    printf("Right-sibling of D: %d\n", right_sibling(3, tree));
-    printf("Right-sibling of E: %d\n", right_sibling(4, tree));
-    printf("Right-sibling of F: %d\n", right_sibling(5, tree));
-    printf("Right-sibling of G: %d\n", right_sibling(6, tree));
-    printf("Right-sibling of H: %d\n", right_sibling(7, tree));
+    printf("Right-sibling of A: %d\n", tree_right_sibling_of(0, tree));
+    printf("Right-sibling of B: %d\n", tree_right_sibling_of(1, tree));
+    printf("Right-sibling of C: %d\n", tree_right_sibling_of(2, tree));
+    printf("Right-sibling of D: %d\n", tree_right_sibling_of(3, tree));
+    printf("Right-sibling of E: %d\n", tree_right_sibling_of(4, tree));
+    printf("Right-sibling of F: %d\n", tree_right_sibling_of(5, tree));
+    printf("Right-sibling of G: %d\n", tree_right_sibling_of(6, tree));
+    printf("Right-sibling of H: %d\n", tree_right_sibling_of(7, tree));
 
-    preorder(root_node(tree), tree); PRINT_NEW_LINE
-    inorder(root_node(tree), tree); PRINT_NEW_LINE
-    postorder(root_node(tree), tree); PRINT_NEW_LINE
+    preorder_tree(tree_root_node(tree), tree);
+    PRINT_NEW_LINE
+    inorder_tree(tree_root_node(tree), tree);
+    PRINT_NEW_LINE
+    postorder_tree(tree_root_node(tree), tree);
+    PRINT_NEW_LINE
 
     return 0;
 }
-
