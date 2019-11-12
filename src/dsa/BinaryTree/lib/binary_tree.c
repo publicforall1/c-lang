49aadad647457c11aa073805764373c3196de72f
taiprogramer <atgtai100@gmail.com>
2019-11-12 09:08:53 +0700

    @compatible: Binary Tree

--- src/dsa/BinaryTree/lib/binary_tree.c
+++ src/dsa/BinaryTree/lib/binary_tree.c
@@ -2,16 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void makenull_btree(B_Tree* root){
-    *root = NULL;
-}
+void makenull_btree(B_Tree* root) { *root = NULL; }
 
-bool is_empty_btree(B_Tree root){
-    return root == NULL;
-}
+bool is_empty_btree(B_Tree root) { return root == NULL; }
 
-B_Tree merge_btree(DataType value, B_Tree first_tree, B_Tree second_tree){
-    B_Tree node = (B_Tree) malloc(sizeof(struct Node));
+B_Tree merge_btree(B_Tree_DataType value, B_Tree first_tree,
+                   B_Tree second_tree) {
+    B_Tree node = (B_Tree)malloc(sizeof(struct B_Tree_Node));
     // worst case: memory allocation got an error
     // how to handle this situation
     // ...........................
@@ -21,42 +18,40 @@ B_Tree merge_btree(DataType value, B_Tree first_tree, B_Tree second_tree){
     return node;
 }
 
-void destroy_btree(B_Tree root){
+void destroy_btree(B_Tree root) {
     if (root == NULL)
-	return;
+        return;
     destroy_btree(root->left);
     destroy_btree(root->right);
     free(root);
 }
 
-DataType get_value(B_Tree node){
+B_Tree_DataType get_value_from_btree(B_Tree node) {
     return (node == NULL ? -1 : node->value);
 }
 
-void preorder_btree(B_Tree root){
+void preorder_btree(B_Tree root) {
     if (root == NULL)
-	return;
+        return;
 
-    printf("%c ", get_value(root));
+    printf("%c ", get_value_from_btree(root));
     preorder_btree(root->left);
     preorder_btree(root->right);
 }
 
-
-void inorder_btree(B_Tree root){
+void inorder_btree(B_Tree root) {
     if (root == NULL)
-	return;
+        return;
 
     inorder_btree(root->left);
-    printf("%c ", get_value(root));
+    printf("%c ", get_value_from_btree(root));
     inorder_btree(root->right);
 }
-void postorder_btree(B_Tree root){
+void postorder_btree(B_Tree root) {
     if (root == NULL)
-	return;
+        return;
 
     postorder_btree(root->left);
     postorder_btree(root->right);
-    printf("%c ", get_value(root));
+    printf("%c ", get_value_from_btree(root));
 }
-
--- src/dsa/BinaryTree/lib/binary_tree.h
+++ src/dsa/BinaryTree/lib/binary_tree.h
@@ -3,24 +3,24 @@
 
 #include <stdbool.h>
 
-typedef char DataType;
+typedef char B_Tree_DataType;
 
-struct Node {
-    DataType value;
-    struct Node* left;
-    struct Node* right;
+struct B_Tree_Node {
+    B_Tree_DataType value;
+    struct B_Tree_Node* left;
+    struct B_Tree_Node* right;
 };
 
-typedef struct Node* B_Tree;
+typedef struct B_Tree_Node* B_Tree;
 
 void makenull_btree(B_Tree* root);
 bool is_empty_btree(B_Tree root);
-B_Tree merge_btree(DataType value, B_Tree first_tree, B_Tree second_tree);
+B_Tree merge_btree(B_Tree_DataType value, B_Tree first_tree,
+                   B_Tree second_tree);
 void destroy_btree(B_Tree root);
-DataType get_value(B_Tree node);
+B_Tree_DataType get_value_from_btree(B_Tree node);
 void preorder_btree(B_Tree root);
 void inorder_btree(B_Tree root);
 void postorder_btree(B_Tree root);
 
 #endif
-
