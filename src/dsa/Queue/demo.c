20fcc1c0f01813476dffabb3855cd9cd1c110f0c
taiprogramer <atgtai100@gmail.com>
2019-11-12 08:28:25 +0700

    @add: Queue - Demo

--- src/dsa/Queue/demo.c
+++ src/dsa/Queue/demo.c
@@ -5,22 +5,23 @@
 #include "lib/queue.h"
 #include <stdio.h>
 
-int main(void){
+int main(void) {
     Queue my_queue;
     makenull_queue(&my_queue);
 
     int total_element = 50;
-    for(int i = 0; i < total_element; ++i){
-	printf("Enqueue #%d\n", i);
-	enqueue(i * 2, &my_queue); 
-    }
 
-    for(int i = 0; i < total_element; ++i){
-	printf("Dequeue #%d\n", i);
-	dequeue(&my_queue);
+    printf("\n============== Enqueue ==============\n");
+    for (int i = 0; i < total_element; ++i) {
+        enqueue(i * 2, &my_queue);
+    }
+    print_queue(my_queue);
+
+    printf("\n============== Dequeue ==============\n");
+    for (int i = 0; i < total_element; ++i) {
+        dequeue(&my_queue);
     }
     print_queue(my_queue);
 
     return 0;
 }
-
