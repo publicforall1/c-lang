ef5b20a403c0f95770dc655c4ff36f5051ecf931
taiprogramer <atgtai100@gmail.com>
2020-02-01 14:28:25 +0700

    Queue: refactor

--- src/dsa/Queue/lib/circular_queue.h
+++ src/dsa/Queue/lib/circular_queue.h
@@ -1,11 +1,11 @@
-#ifndef CIRCULAR_QUEUE_H
-#define CIRCULAR_QUEUE_H
+#ifndef QUEUE_H
+#define QUEUE_H
 
 #define MAX_LENGTH_OF_QUEUE 100
 
 typedef int Queue_ElementType;
 
-typedef struct Circular_Queue {
+typedef struct {
     Queue_ElementType value[MAX_LENGTH_OF_QUEUE];
     int front, rear;
 } Queue;
